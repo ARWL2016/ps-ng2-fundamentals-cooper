8a40778c626db35b1d065d517f8542c01b226fc7
Alistair Willis <alistairrwillis@gmail.com>
2017-04-18 21:35:15 +0900

    add event details component

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,9 @@ import {EventsAppComponent} from "./events-app.component";
 import {EventsListComponent} from "./events/events-list.component";
 import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 import { NavBarComponent } from "./nav/navbar.component";
+import { EventService } from "./events/shared/event.service";
+import { ToastrService } from "./common/toastr.service";
+import { EventDetailsComponent } from "./events/event-details/event-details.component";
 
 
 @NgModule({
@@ -15,8 +18,10 @@ import { NavBarComponent } from "./nav/navbar.component";
     EventsAppComponent, 
     NavBarComponent,
     EventsListComponent, 
-    EventThumbnailComponent
+    EventThumbnailComponent, 
+    EventDetailsComponent
   ], 
-  bootstrap: [EventsAppComponent]
+  providers: [ EventService, ToastrService ], 
+  bootstrap: [ EventsAppComponent ]
 })
 export class AppModule{}
