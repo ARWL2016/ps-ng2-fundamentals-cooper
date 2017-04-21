7bc4fe598438d6ee538f1369da8aa1e0aff3bf4c
Alistair Willis <alistairrwillis@gmail.com>
2017-04-21 13:19:55 +0900

    add filter by function

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -15,6 +15,8 @@ import { ISession } from "../index";
 export class EventDetailsComponent {
   event:IEvent; 
   addMode: boolean; 
+  filterBy: string = 'all'; 
+  sortBy: string =  'votes'; 
   
   constructor(private eventService:EventService, private route:ActivatedRoute) {
 
--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -1,10 +1,32 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnChanges } from '@angular/core'
 import { ISession } from '../shared/index'
 
 @Component({
   selector: 'session-list',
   templateUrl: 'app/events/event-details/session-list.component.html'
 })
-export class SessionListComponent {
-  @Input() sessions:ISession[]
+export class SessionListComponent implements OnChanges {
+  @Input() sessions:ISession[]; 
+  @Input() filterBy: string; 
+  @Input() sortBy: string; 
+  visibleSessions: ISession[] =[]; 
+
+  ngOnChanges():void {
+    if(this.sessions) {
+      this.filterSessions(this.filterBy); 
+      this.sortBy === 'name' ? 
+        this.visibleSessions.sort(sortByNameAsc) :
+        this.visibleSessions.sort(sortByVotesDesc); 
+    }
+  }
+
+  filterSessions(filter:string) {
+    if (filter === 'all') {
+      this.visibleSessions = this.sessions.slice(0); 
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter; 
+      })
+    }
+  }
 }
